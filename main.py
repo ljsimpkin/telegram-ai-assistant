8995769aaa6c7f48047d5983af0896095459c4db
liam <ljsimpkin@gmail.com>
2023-08-19 13:25:00 +1200

    remove download import

--- main.py
+++ main.py
@@ -26,8 +26,6 @@ async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text_caps = ' '.join(context.args).upper()
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
-from telegram import download
-
 async def voice_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     file_id = update.message.voice.file_id
     new_file = await context.bot.get_file(file_id)
