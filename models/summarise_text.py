099c56bb6b17a62b7c029580ca2bf76d17f32b7b
liam <ljsimpkin@gmail.com>
2023-08-20 09:42:12 +1200

    remove token

--- models/summarise_text.py
+++ models/summarise_text.py
@@ -1,8 +1,6 @@
 import requests
 from env import HUGGING_FACE_TOKEN
 
-HUGGING_FACE_TOKEN = "Bearer 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"
-
 API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
 headers = {"Authorization": HUGGING_FACE_TOKEN}
 
