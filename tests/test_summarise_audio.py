c30f6e918b81f011a5c34b6c0989584fe5885eb3
liam <ljsimpkin@gmail.com>
2023-08-19 21:42:30 +1200

    aider: Import `summariseText` function from `models.summarise_text` into `tests/test_summarise_audio.py` and add a new test function to ensure the function is working as expected.
    # Aider chat conversation:
    
    USER: how do you import a function into pytest
    
    ASSISTANT: To import a function into a pytest test file, you simply use the standard Python import statement.
    
    For example, if you have a function named `my_function` in a file named `my_module.py`, you would import it into your test file like this:
    
    ```python
    from my_module import my_function
    ```
    
    Then, you can use `my_function` in your tests.
    
    If `my_module.py` is in a different directory, you need to include the path in the import statement. For example, if `my_module.py` is in a directory named `my_directory`, you would import `my_function` like this:
    
    ```python
    from my_directory.my_module import my_function
    ```
    
    Remember that the directory containing `my_module.py` must be a Python package, which means it needs to contain a file named `__init__.py` (this file can be empty).
    
    USER: I added these *read-write* files: models/__init__.py
    
    ASSISTANT: Great! Now that `models/__init__.py` is a *read-write* file, we can import the `summariseText` function from `models.summarise_text` into the `tests/test_summarise_audio.py` file.
    
    Here is the *edit block* for the changes:
    
    ```python
    tests/test_summarise_audio.py
    <<<<<<< HEAD
    from models.summarise_text import summariseText
    
    output = summariseText({
    	"inputs": "there were 3 cats and the dinner wasn't prepared forthem. it was unfortuante you had to come late",
    })
    
    print(output)
    =======
    from models.summarise_text import summariseText
    
    def test_summarise_text_is_true():
        output = summariseText({
            "inputs": "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.",
        })
        assert output is not None
    >>>>>>> updated
    ```
    
    This new test function will call the `summariseText` function with a sample input and assert that the output is not None. This is a basic test to ensure that the function is working as expected.

--- tests/test_summarise_audio.py
+++ tests/test_summarise_audio.py
@@ -1,11 +1,17 @@
 from models.summarise_text import summariseText
 
-def test_true_is_true():
-    assert True is True
-
 def test_summarise_text_is_true():
     output = summariseText({
         "inputs": "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.",
     })
     assert output is not None
+
+# def test_true_is_true():
+#     assert True is True
+
+# def test_summarise_text_is_true():
+#     output = summariseText({
+#         "inputs": "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.",
+#     })
+#     assert output is not None
 
