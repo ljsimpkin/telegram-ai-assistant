c2cf781983a47033ce6737a12691c4ae5b129371
liam <ljsimpkin@gmail.com>
2023-08-19 17:44:18 +1200

    rename variable

--- models/hugging_face.py
+++ models/hugging_face.py
@@ -1,14 +1,14 @@
 import requests
 from env import HUGGING_FACE_TOKEN
 
-API_URL = "https://api-inference.huggingface.co/models/openai/whisper-base"
+TRANSCRIPTION_API_URL = "https://api-inference.huggingface.co/models/openai/whisper-base"
 IMAGE_API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
 headers = {"Authorization": HUGGING_FACE_TOKEN}
 
 def query(filename):
     with open(filename, "rb") as f:
         data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data)
+    response = requests.post(TRANSCRIPTION_API_URL, headers=headers, data=data)
     return response.json()
 
 def get_image(payload):
