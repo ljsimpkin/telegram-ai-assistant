2aef0137777d5b67ef9b25ca9bde066cac16fdff
liam <ljsimpkin@gmail.com>
2023-08-20 09:57:27 +1200

    use gpt chat for text summarization

--- commands/voice_handler.py
+++ commands/voice_handler.py
@@ -1,9 +1,10 @@
 from .common import Update, ContextTypes
 from models.transcribe_audio import transcribe_audio
-from models.summarise_text import summarise_text
+from models.summarize_gpt import summarize_text
 
 async def voice_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = await transcribe_audio(update, context)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message["text"])
-    summary = await summarise_text(message["text"])
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=summary[0]["summary_text"])
+    # import pdb; pdb.set_trace()
+    summary = summarize_text(message["text"])
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=summary)
