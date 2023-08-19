2654ec4140e8d894500c14dbeeb2a8d23c8b09d4
liam <ljsimpkin@gmail.com>
2023-08-20 11:30:59 +1200

    Increase the character limit for the returned content to 4000.

--- models/getHTML.py
+++ models/getHTML.py
@@ -6,4 +6,4 @@ def get_static_website(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     content = soup.find(id='mw-content-text')
-    return content.get_text()[:2000]
+    return content.get_text()[:4000]
