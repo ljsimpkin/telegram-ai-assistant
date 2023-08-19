53a9278b9b1826ca4d9ef3808f6b9dfb23aa0260
liam <ljsimpkin@gmail.com>
2023-08-20 01:16:11 +1200

    Delete test_summarize_api.py test file.

--- # tests/test_summarize_api.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from api.summarize_api import summarize_text
-
-def test_summarize_text():
-    text = "This is a long text that needs to be summarized."
-    summary = summarize_text(text)
-    assert len(summary) < len(text), "The summary is not shorter than the original text."
-    assert summary != "", "The summary is empty."
