5337e6220bfa2591618770390cf76d3b5497e1b8
liam <ljsimpkin@gmail.com>
2023-08-20 01:23:42 +1200

    aider: Enable the test to print the output of the summarize_text function to the terminal.
    # Aider chat conversation:
    
    USER: enable this test to print the ouput of the summarize_text function to the terminal
    
    ASSISTANT: To enable the test to print the output of the `summarize_text` function to the terminal, we need to modify the `test_summarize_text` function in the `demo/test_summarize_api.py` file.
    
    Here is the change:
    
    ```python
    demo/test_summarize_api.py
    <<<<<<< HEAD
    def test_summarize_text():
        print("\n\n\n amazing this is where its at \n\n\n")
        text = "This is a long text that needs to be summarized."
        summary = summarize_text(text)
        assert len(summary) < len(text), "The summary is not shorter than the original text."
        assert summary != "", "The summary is empty."
    =======
    def test_summarize_text():
        print("\n\n\n amazing this is where its at \n\n\n")
        text = "This is a long text that needs to be summarized."
        summary = summarize_text(text)
        print(f"\n\n\nSummary: {summary}\n\n\n")  # print the summary
        assert len(summary) < len(text), "The summary is not shorter than the original text."
        assert summary != "", "The summary is empty."
    >>>>>>> updated
    ```
    
    This change adds a print statement that outputs the summary to the terminal.

--- demo/test_summarize_api.py
+++ demo/test_summarize_api.py
@@ -5,5 +5,6 @@ def test_summarize_text():
     print("\n\n\n amazing this is where its at \n\n\n")
     text = "This is a long text that needs to be summarized."
     summary = summarize_text(text)
+    print(f"\n\n\nSummary: {summary}\n\n\n")  # print the summary
     assert len(summary) < len(text), "The summary is not shorter than the original text."
     assert summary != "", "The summary is empty."
