5d11c7392e9528e34237b2fcd06c1927c74aade7
liam <ljsimpkin@gmail.com>
2023-08-19 18:25:20 +1200

    refactor into models and commands

--- commands/voice_handler.py
+++ commands/voice_handler.py
@@ -1,30 +1,7 @@
 import os
 from .common import Update, ContextTypes
-from models.hugging_face import query, get_image
-
-def save_image(image_bytes, filename):
-    with open(filename, 'wb') as f:
-        f.write(image_bytes)
-        
-def text_to_image(text):
-  image_bytes = get_image({
-    "inputs": text,
-  })
-  return save_image(image_bytes,  './downloads/output_image.jpg')
+from models.hugging_face import summarise_audio, test
 
 async def voice_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # download the voice message
-    file_id = update.message.voice.file_id
-    new_file = await context.bot.get_file(file_id)
-    await new_file.download_to_drive('downloads/voice_message.ogg')
-    # transcribe the voice message
-    output = query("./downloads/voice_message.ogg")
-    # respond with the transcription
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=output["text"]
-    )
-    # Generate an image from the transcription
-    text_to_image(output["text"])
-    # respond with the generated image
-    await context.bot.send_document(chat_id=update.effective_chat.id, document='./downloads/output_image.jpg')
+    message = await summarise_audio(update, context)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message["text"])
--- models/hugging_face.py
+++ models/hugging_face.py
@@ -1,6 +1,10 @@
 import requests
 from env import HUGGING_FACE_TOKEN
 
+import os
+from telegram import Update
+from telegram.ext import ContextTypes
+
 TRANSCRIPTION_API_URL = "https://api-inference.huggingface.co/models/openai/whisper-base"
 IMAGE_API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
 headers = {"Authorization": HUGGING_FACE_TOKEN}
@@ -14,3 +18,24 @@ def query(filename):
 def get_image(payload):
 	response = requests.post(IMAGE_API_URL, headers=headers, json=payload)
 	return response.content
+
+def save_image(image_bytes, filename):
+    with open(filename, 'wb') as f:
+        f.write(image_bytes)
+        
+def text_to_image(text):
+  image_bytes = get_image({
+    "inputs": text,
+  })
+  return save_image(image_bytes,  './downloads/output_image.jpg')
+
+def test():
+  return "\n\n\ntest\n\n\n"
+
+async def summarise_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # download the voice message
+    file_id = update.message.voice.file_id
+    new_file = await context.bot.get_file(file_id)
+    await new_file.download_to_drive('downloads/voice_message.ogg')
+    # transcribe the voice message
+    return query("./downloads/voice_message.ogg")
