23b372c538610ce990ef1dd916d80b289c33e33d
liam <ljsimpkin@gmail.com>
2023-08-19 19:17:04 +1200

    aider: Added a test case to test that the `summarise_audio` function returns text.
    # Aider chat conversation:
    
    USER: write a test that tests summarise_audio returns text
    
    ASSISTANT: Sure, we can add a test case to the `test_summarise_audio.py` file to test that the `summarise_audio` function returns text.
    
    Here is the updated test file:
    
    ```python
    tests/test_summarise_audio.py
    <<<<<<< HEAD
    import pytest
    from models.summarise_audio import query, summarise_audio
    
    def test_query():
        # TODO: Add test cases for the query function
    
    def test_summarise_audio():
        # TODO: Add test cases for the summarise_audio function
    =======
    import pytest
    from models.summarise_audio import query, summarise_audio
    from unittest.mock import Mock, patch
    
    def test_query():
        # TODO: Add test cases for the query function
    
    @patch('models.summarise_audio.query')
    def test_summarise_audio(mock_query):
        # Mock the query function to return a specific value
        mock_query.return_value = "This is a test transcription"
    
        # Mock the update and context objects
        mock_update = Mock()
        mock_context = Mock()
    
        # Call the summarise_audio function
        result = summarise_audio(mock_update, mock_context)
    
        # Assert that the result is a string (text)
        assert isinstance(result, str)
    >>>>>>> updated
    ```
    
    This test case uses the `unittest.mock` library to mock the `query` function and the `update` and `context` objects. The `query` function is mocked to return a specific value, and then we call the `summarise_audio` function with the mocked objects. Finally, we assert that the result is a string, which means that the `summarise_audio` function returns text.

--- tests/test_summarise_audio.py
+++ tests/test_summarise_audio.py
@@ -1,8 +1,21 @@
 import pytest
 from models.summarise_audio import query, summarise_audio
+from unittest.mock import Mock, patch
 
 def test_query():
     # TODO: Add test cases for the query function
 
-def test_summarise_audio():
-    # TODO: Add test cases for the summarise_audio function
+@patch('models.summarise_audio.query')
+def test_summarise_audio(mock_query):
+    # Mock the query function to return a specific value
+    mock_query.return_value = "This is a test transcription"
+    
+    # Mock the update and context objects
+    mock_update = Mock()
+    mock_context = Mock()
+    
+    # Call the summarise_audio function
+    result = summarise_audio(mock_update, mock_context)
+    
+    # Assert that the result is a string (text)
+    assert isinstance(result, str)
