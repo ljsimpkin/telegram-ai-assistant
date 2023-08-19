848b28c66aa84d35b22a159635fa9fd070b771ed
liam <ljsimpkin@gmail.com>
2023-08-20 10:38:13 +1200

    gpt prompt

--- commands/voice_handler.py
+++ commands/voice_handler.py
@@ -6,4 +6,5 @@ async def voice_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = await transcribe_audio(update, context)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message["text"])
     summary = summarize_text(message["text"])
+    summary = "Summary: " + summary
     await context.bot.send_message(chat_id=update.effective_chat.id, text=summary)
--- models/summarize_gpt.py
+++ models/summarize_gpt.py
@@ -5,8 +5,9 @@ MODEL="gpt-3.5-turbo"
 MAX_TOKENS=256
 TEMPERATURE=1
 
-CODE_FLAG="You are a bot that summarises the users input. Reduce it half the length"
-# CODE_FLAG="Increase the length of the user input"
+# CODE_FLAG="You are a bot that summarises the users input. Reduce it half the length"
+
+# CODE_FLAG="As a professional summarizer, create a concise and comprehensive summary of the provided text, be it an article, post, conversation, or passage, while adhering to these guidelines: 1 Craft a summary that is detailed, thorough, in-depth, and complex, while maintaining clarity and conciseness. 2 Incorporate main ideas and essential information, eliminating extraneous language and focusing on critical aspects. 3 Rely strictly on the provided text, without including external information. 4 Format the summary in paragraph form for easy understanding. By following this optimized prompt, you will generate an effective summary that encapsulates the essence of the given text in a clear, concise, and reader-friendly manner."
 
 def setup_openai():
     api_key = os.environ.get("OPENAI_API_KEY")
@@ -23,7 +24,14 @@ def interact_with_gpt(messages):
     )
     return response['choices'][0]['message']['content'].strip()
 
+def sentence_count(string):
+    return len(string.split('.')) 
+
 def summarize_text(text):
+    count = sentence_count(text)
+    length = f'{count // 5} sentences' if count > 3 else "1 sentence"
+    # import pdb; pdb.set_trace()
+    CODE_FLAG= f'You are a bot that summarises the users input. Reduce it down to {length}'
     input_messages=[{'role':'system', 'content': CODE_FLAG}, {"role": "user", "content": text}]
     response = interact_with_gpt(input_messages)
     # This is a placeholder implementation. We'll replace this with a call to the actual API.
