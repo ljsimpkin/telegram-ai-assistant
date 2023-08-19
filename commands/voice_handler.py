cff9e551323c25b928a07acf533d63a4c60b7341
liam <ljsimpkin@gmail.com>
2023-08-20 00:43:59 +1200

    summarise text from voice message

--- commands/voice_handler.py
+++ commands/voice_handler.py
@@ -1,7 +1,9 @@
-import os
 from .common import Update, ContextTypes
-from models.summarise_audio import summarise_audio
+from models.transcribe_audio import transcribe_audio
+from models.summarise_text import summarise_text
 
 async def voice_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message = await summarise_audio(update, context)
+    message = await transcribe_audio(update, context)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message["text"])
+    summary = await summarise_text(message["text"])
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=summary[0]["summary_text"])
--- models/summarise_text.py
+++ models/summarise_text.py
@@ -6,12 +6,8 @@ HUGGING_FACE_TOKEN = "Bearer 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"
 API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
 headers = {"Authorization": HUGGING_FACE_TOKEN}
 
-def summarise_text(payload):
+async def summarise_text(text):
+	payload = {"inputs": text}
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
 
-output = summarise_text({
-	"inputs": "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building",
-})
-
-print(output)
