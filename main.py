a5acb7fec138ddd5a8ef3f405bf33f5f8b717a42
liam <ljsimpkin@gmail.com>
2023-08-19 13:55:57 +1200

    add audio transcription

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import os
 from telegram import Update, InputTextMessageContent
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
+import requests
+from env import TELEGRAM_TOKEN, HUGGING_FACE_TOKEN
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+API_URL = "https://api-inference.huggingface.co/models/openai/whisper-tiny"
+headers = {"Authorization": HUGGING_FACE_TOKEN}
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
 
 # Create a directory for voice messages if it doesn't exist
 if not os.path.exists('voice_messages'):
@@ -35,14 +46,20 @@ async def voice_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     file_id = update.message.voice.file_id
     new_file = await context.bot.get_file(file_id)
     await new_file.download_to_drive('voice_messages/voice_message.ogg')
+    output = query("./voice_messages/voice_message.ogg")
+    # import pdb; pdb.set_trace()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=output["text"]
+    )
+
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 
 if __name__ == '__main__':
-    from env import TOKEN
-    application = ApplicationBuilder().token(TOKEN).build()
+    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(echo_handler)
