36f019842f5b05c4e4ec72aa57098ebdf02cccba
liam <ljsimpkin@gmail.com>
2023-08-20 09:57:48 +1200

    refactor

--- commands/voice_handler.py
+++ commands/voice_handler.py
@@ -5,6 +5,5 @@ from models.summarize_gpt import summarize_text
 async def voice_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = await transcribe_audio(update, context)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message["text"])
-    # import pdb; pdb.set_trace()
     summary = summarize_text(message["text"])
     await context.bot.send_message(chat_id=update.effective_chat.id, text=summary)
