0473ec3c4890b37cfa4a5d27b9193d48eb009fd5
liam <ljsimpkin@gmail.com>
2023-08-22 23:21:24 +1200

    working readible text extractor

--- commands/summarize_url.py
+++ commands/summarize_url.py
@@ -1,24 +1,20 @@
 from .common import Update, ContextTypes
 from models.transcribe_audio import transcribe_audio
 from models.summarize_gpt import summarize_text
-from models.getHTML import get_static_website
+from models.get_html import get_static_website, get_readable
 
 async def summarize_url(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print ("\n\nurl summariser lesh gooo\n\n")
 
     # URL = "https://en.wikipedia.org/wiki/New_Zealand"
     URL = ' '.join(context.args)
-    page = get_static_website(URL)
+    # page = get_static_website(URL)
+    page = get_readable(URL)[:3000]
 
     # import pdb; pdb.set_trace()
 
     await context.bot.send_message(chat_id=update.effective_chat.id, text=page)
 
-    summary = summarize_text(page)
-
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=summary)
-    # message = await transcribe_audio(update, context)
-    # await context.bot.send_message(chat_id=update.effective_chat.id, text=message["text"])
-    # 
+    # summary = summarize_text(page)
     # summary = "Summary: " + summary
     # await context.bot.send_message(chat_id=update.effective_chat.id, text=summary)
--- models/get_html.py
+++ models/get_html.py
@@ -22,7 +22,7 @@ def clean_text(text):
     text = re.sub(r'\n+', '\n', text)  # remove multiple new lines
     text = re.sub(r'\[\d+\]', '', text)  # remove references like [1], [2], etc.
     text = re.sub(r'\t', '', text)  # remove tabs
-    text = re.sub(r'  ', ' ', text)  # replace double spaces with single space
+    text = re.sub(r'  ', '', text)  # replace double spaces with nothing
     return text.strip()  # remove leading and trailing whitespace
 
 def get_readable(url):
@@ -32,6 +32,10 @@ def get_readable(url):
   summary_html = doc.summary()
   return clean_text(parse_html_to_text(summary_html))
 
-url = "https://edition.cnn.com/2023/08/21/travel/jetzero-blended-wing-plane-climate-spc/index.html"
+# url = "https://edition.cnn.com/2023/08/21/travel/jetzero-blended-wing-plane-climate-spc/index.html"
 # url = "https://en.wikipedia.org/wiki/New_Zealand"
-print (get_readable(url))
+url = "https://www.technologyreview.com/2023/08/18/1077537/menstruation-mystery/"
+# print (get_readable(url))
+
+# import html2text
+# print (html2text.html2text(url))
