1a2c6478a2b9923e964765599d4cc4329156e8ef
liam <ljsimpkin@gmail.com>
2023-12-17 10:42:03 +1300

    aider: Remove manual modification of state and user_data, use context.user_data to store and retrieve the state in gpt_command.
    # Aider chat conversation:
    
    USER: Manually modifying user_data is almost never needed and unadvisable. make the gpt_command add message_text to the user's state doing it the python telegram way
    
    ASSISTANT: To make this change, we need to modify `commands/gpt.py`:
    
    1. Remove the manual modification of `state` and `user_data`.
    2. Use the `context.user_data` to store and retrieve the state.
    
    Here is the *SEARCH/REPLACE* block:
    
    commands/gpt.py
    ```python
    <<<<<<< SEARCH
    state = context.user_data['state']
        chat_id = update.effective_chat.id
        if chat_id not in state:
            state[chat_id] = {}
    
        if "message" not in state[chat_id]:
            state[chat_id]["message"] = []
    
        # Remove the command from the message text e.g. /gpt
        message_text = ' '.join(update.message.text.split()[1:])
    
        state[chat_id]["message"].append({"role": "user", "content": message_text})
    
        response = await gpt_start(state[chat_id])
        await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
    =======
        chat_id = update.effective_chat.id
        if 'state' not in context.user_data:
            context.user_data['state'] = {}
    
        if chat_id not in context.user_data['state']:
            context.user_data['state'][chat_id] = {"message": []}
    
        # Remove the command from the message text e.g. /gpt
        message_text = ' '.join(update.message.text.split()[1:])
    
        context.user_data['state'][chat_id]["message"].append({"role": "user", "content": message_text})
    
        response = await gpt_start(context.user_data['state'][chat_id])
        await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
    >>>>>>> REPLACE
    ```

--- commands/gpt.py
+++ commands/gpt.py
@@ -2,18 +2,17 @@ from .common import Update, ContextTypes
 from models.gpt_conversation import gpt_start
 
 async def gpt_command(update: Update, context: ContextTypes.context):
-    state = context.user_data['state']
         chat_id = update.effective_chat.id
-        if chat_id not in state:
-            state[chat_id] = {}
+        if 'state' not in context.user_data:
+            context.user_data['state'] = {}
 
-        if "message" not in state[chat_id]:
-            state[chat_id]["message"] = []
+        if chat_id not in context.user_data['state']:
+            context.user_data['state'][chat_id] = {"message": []}
 
         # Remove the command from the message text e.g. /gpt
         message_text = ' '.join(update.message.text.split()[1:])
 
-        state[chat_id]["message"].append({"role": "user", "content": message_text})
+        context.user_data['state'][chat_id]["message"].append({"role": "user", "content": message_text})
 
-        response = await gpt_start(state[chat_id])
+        response = await gpt_start(context.user_data['state'][chat_id])
         await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
