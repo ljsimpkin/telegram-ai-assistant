5ac6ec9d145d5b2bf3fc856e84fa7e338a650a25
liam <ljsimpkin@gmail.com>
2024-01-07 23:22:11 +1300

    rename state cleared to conversation cleared

--- commands/state_clear.py
+++ commands/state_clear.py
@@ -2,4 +2,4 @@ from .common import Update, ContextTypes
 
 async def clear_state(update: Update, context: ContextTypes.context):
   context.user_data['state'] = {}
-  await context.bot.send_message(chat_id=update.effective_chat.id, text="State Cleared")
+  await context.bot.send_message(chat_id=update.effective_chat.id, text="Conversation cleared")
